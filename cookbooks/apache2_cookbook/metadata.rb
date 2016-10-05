ade3631eb9536d0f63441d1a3494ce430a3cbfe4
jaspardzc <jaspardzc@gmail.com>
2016-10-04 23:53:18 -0700

    created apache2_cookbook

---
+++ cookbooks/apache2_cookbook/metadata.rb
@@ -0,0 +1,15 @@
+name 'apache2_cookbook'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'all_rights'
+description 'Installs/Configures apache2_cookbook'
+long_description 'Installs/Configures apache2_cookbook'
+version '0.1.0'
+
+# If you upload to Supermarket you should set this so your cookbook
+# gets a `View Issues` link
+# issues_url 'https://github.com/<insert_org_here>/apache2_cookbook/issues' if respond_to?(:issues_url)
+
+# If you upload to Supermarket you should set this so your cookbook
+# gets a `View Source` link
+# source_url 'https://github.com/<insert_org_here>/apache2_cookbook' if respond_to?(:source_url)
