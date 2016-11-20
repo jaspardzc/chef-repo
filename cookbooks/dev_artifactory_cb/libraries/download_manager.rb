ac98404e8ac9a2f9572f3a3f9c2a4ae9bb92056f
jaspardzc <jaspardzc@gmail.com>
2016-11-20 13:47:27 -0700

    updated artifactory cookbook

--- cookbooks/dev_artifactory_cb/libraries/download_manager.rb
+++ cookbooks/dev_artifactory_cb/libraries/download_manager.rb
@@ -3,25 +3,25 @@
 
 class DownloadManager
 
-	@@localuri
+    @@localuri
     @@localmeta
-	@@remoteuri
+    @@remoteuri
     @@remotemeta
     @@latest_version
 
-	def initialize(_localuri, _remoteuri, _localmeta, _remotemeta)
-		# assign instance variables
+    def initialize(_localuri, _remoteuri, _localmeta, _remotemeta)
+        # assign instance variables
 
-		@@localuri = _localuri
+        @@localuri = _localuri
         @@localmeta = _localmeta
-		@@remoteuri = _remoteuri
+        @@remoteuri = _remoteuri
         @@remotemeta = _remotemeta
         @@latest_version = ""
-	end
+    end
 
-	def self.getLocalUri()
-		return @@localuri
-	end
+    def self.getLocalUri()
+        return @@localuri
+    end
 
     def self.getLocalMeta()
         return @@localmeta
@@ -31,9 +31,9 @@ class DownloadManager
         return @@remotemeta
     end
 
-	def self.getRemoteUri()
-		return @@remoteuri
-	end
+    def self.getRemoteUri()
+        return @@remoteuri
+    end
 
     # a HTML Parser function to find and set the latest verison of wanted artifact
     def self.setLatestVersion(spname)
@@ -55,22 +55,22 @@ class DownloadManager
 
         # iterate the input array and filters out the string contains .war information
         for i in input_array  
-    	    myString = String.new("#{i}")
-	
-	        # push the string into the result_array if the string contains substring '.war</a>'
-	        if myString.include?war_pattern
-		        result_array.push(myString)
-	        end
+            myString = String.new("#{i}")
+    
+            # push the string into the result_array if the string contains substring '.war</a>'
+            if myString.include?war_pattern
+                result_array.push(myString)
+            end
         end
 
         # iterate the result array, find out the the string that has larger timestamp appended, 
         # extract the value of the string and assign to a local variable
         for j in result_array
-	        splitted_array_1  = j.split(pattern=string_pattern_1)
-	        temp = splitted_array_1[1]
-	        splitted_array_2 = temp.split(pattern=string_pattern_2)
-	        temp = splitted_array_2[0]
-	        sorted_result_array.push(temp)
+            splitted_array_1  = j.split(pattern=string_pattern_1)
+            temp = splitted_array_1[1]
+            splitted_array_2 = temp.split(pattern=string_pattern_2)
+            temp = splitted_array_2[0]
+            sorted_result_array.push(temp)
         end
 
         # sorting the array by reverse order
--- cookbooks/dev_artifactory_cb/recipes/default.rb
+++ cookbooks/dev_artifactory_cb/recipes/default.rb
@@ -1,10 +1,11 @@
-#
-# Cookbook: dev_artifactory_cb
-# Recipe: default
-#
-# Copyright (c) 2016 The Authors, All Rights Reserved.
-
-# Keep Clean
+####################################################################################
+# Cookbook Name: dev_artifactory_cb
+# Recipe:: default
+# Strategy: keep clean
+# Copyright (c) 2016 The Auhtors, All Rights Reserved
+# Last Updated: 11/19/2016
+# Author: kevin.zeng
+#####################################################################################
 
 Chef::Log.info("this is dev_artifactory_cb default recipe");
 
